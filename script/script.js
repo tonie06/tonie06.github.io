251fd48ba1d12ec7994644ea63c060dd92e4a53a
Tonie <95538243+tonie06@users.noreply.github.com>
2022-08-12 00:53:13 +0000

    Version 3.4

--- script/script.js
+++ script/script.js
@@ -1,1 +1,7 @@
-AOS.init();
+function myFunction() {
+  var copyText = document.getElementById("myInput");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+  alert("Copied the text: " + copyText.value);
+}
