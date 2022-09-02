15236bc6f5153faeb33d5d0a319cc0ee356b7b2a
Tonie <95538243+tonie06@users.noreply.github.com>
2022-09-03 02:19:35 +0800

    Version 13.3

---
+++ script/script.js
@@ -0,0 +1,6 @@
+kofiWidgetOverlay.draw('tonie', {
+    'type': 'floating-chat',
+    'floating-chat.donateButton.text': 'Support',
+    'floating-chat.donateButton.background-color': '#eacda3',
+    'floating-chat.donateButton.text-color': '#fff'
+  });
