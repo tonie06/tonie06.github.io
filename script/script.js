8eb637214d262fe302796cb2bbde404a2514132f
Tonie <95538243+tonie06@users.noreply.github.com>
2022-09-03 04:24:10 +0800

    Version 13.8

--- script/script.js
+++ script/script.js
@@ -1,6 +1,15 @@
-kofiWidgetOverlay.draw('tonie', {
-    'type': 'floating-chat',
-    'floating-chat.donateButton.text': 'Support',
-    'floating-chat.donateButton.background-color': '#eacda3',
-    'floating-chat.donateButton.text-color': '#fff'
-  });
+const cursorRounded = document.querySelector('.roundedc');
+const cursorPointed = document.querySelector('.pointedc');
+
+
+const moveCursor = (e)=> {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+   
+  cursorRounded.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  
+  cursorPointed.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+ 
+}
+
+window.addEventListener('mousemove', moveCursor)
